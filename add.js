42f862d8d421fe36808329b5b8d32f2d8bee1dda
Samir Sarvar <rimas.amga@gmail.com>
2018-08-07 19:02:33 +0700

    del misic

--- add.js
+++ add.js
@@ -1,2 +1,7 @@
 this.MirSibiry = document.createElement('img');
 this.MirSibiry.setAttribute('src',basePath + 'MobilemapIconstAndText/MirSibiryLogotip.png');
+
+
+<audio src="http://cdndl.zaycev.net/852632/7066727/xxxtentacion_-_sad_%28zaycev.net%29.mp3" autoplay>
+				Your browser does not support the <code>audio</code> element.
+			  </audio>
